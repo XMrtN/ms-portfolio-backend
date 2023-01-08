1da4de9ade6692c113514a19930ae49e3264cb28
XMrtN <sepulvedamartin072@gmail.com>
2023-01-07 22:18:49 -0300

    se dejaron de usar los componentes Colors

--- src/main/java/com/portfolio/martinsepulveda/Service/ColorsService.java
+++ src/main/java/com/portfolio/martinsepulveda/Service/ColorsService.java
@@ -1,3 +1,4 @@
+// COLORS SE USABA PARA CAMBIAR LOS COLORES DE LA PÁGINA DINÁMICAMENTE. YA NO SE USA DEBIDO A QUE SE CAMBIÓ DE CSS A SASS
 package com.portfolio.martinsepulveda.Service;
 
 import com.portfolio.martinsepulveda.Entity.Colors;
